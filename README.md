679bd6ed3569f2905bec603dc7566fa03d643fed
johntalton <johntalton@gmail.com>
2025-08-25 05:59:01 -0400

    modernization first pass

--- README.md
+++ README.md
@@ -1,60 +1,38 @@
-# Fujitsu MB85 FRAM 
+# Fujitsu MB85 FRAM
 
 Simple wrapper around I²C (i2c-bus) implementation to provide read / write capability.
 
 [![npm Version](https://img.shields.io/npm/v/@johntalton/mb85.svg)](https://www.npmjs.com/package/@johntalton/mb85)
 ![GitHub package.json version](https://img.shields.io/github/package-json/v/johntalton/mb85)
-![CI](https://github.com/johntalton/mb85/workflows/CI/badge.svg?branch=master&event=push)
+![CI](https://github.com/johntalton/mb85/workflows/CI/badge.svg)
 ![CodeQL](https://github.com/johntalton/mb85/workflows/CodeQL/badge.svg)
-![GitHub](https://img.shields.io/github/license/johntalton/mb85)
-[![Downloads Per Month](https://img.shields.io/npm/dm/@johntalton/mb85.svg)](https://www.npmjs.com/package/@johntalton/mb85)
-![GitHub last commit](https://img.shields.io/github/last-commit/johntalton/mb85)
-[![Package Quality](https://npm.packagequality.com/shield/%40johntalton%2Fmb85.svg)](https://packagequality.com/#?package=@johntalton/mb85)
+
+
 
 ## API
-Construction is achived via the statis `from` and `fromId` methods of `MB85RC`. 
-```javascript
-const id = ... // detected via I²C, or user specified 
-const fram = MB85RC.fromId(bus, id)
-```
 
-#### Feature string
-The feature set exposed as the Fujitsu product codes.
-
-#### `size` 
-The density expressed as numeric value.
+#### `size`
+The density expressed as numeric bytes value.
 
 #### `densityHuman`
-The density expressed as prodcut code string (example: '1M', '512K', ect)
+The density expressed as product code string (example: '1M', '512K', ect)
 
 #### `busHuman`
 The bus as product code string ('RC' for I²C)
 
-#### `read(offset, length)`
+#### `async read(offset, length)`
 Direct access to read at an offset for desired length.
-Retruning a `Promise` that resolves to a Buffer.
 
-#### `write(offset, buffer)`
-Direct write to an offset given a buffer of desired length to be writen.
-Returning a `Promise` that resolves when completed.
-
-## Devcie ID
-The provided library is also provides a way of identifying a chip based on its assigned Id value.
-
-This can be passed in, however, it becomes more usefull with the bus level `deviceId` call.  This uses a bus level discovery not supported by most chips.  By using this method, the chips feature inforamtion can detected.
+#### `async write(offset, buffer)`
+Direct write to an offset given a buffer of desired length to be written.
 
 ```javascript
-const i2c = require('i2c-bus')
-const { I2CAddressedBus } = require('@johntalton/and-other-delights')
-const { MB85RC, DEFAULT_MB85_ADDRESS } = require('@johntalton/mb85')
+import { I2CAddressedBus } = from '@johntalton/and-other-delights'
+import { MB85RC, DEFAULT_MB85_ADDRESS } from '@johntalton/mb85'
 
-const busNumber = 1
-const busAddress = DEFAULT_MB85_ADDRESS // 0x50
-
-const bus1 = await i2c.openPromisified(busNumber)
-const id = await bus1.deviceId()
-const ab = new I2CAddressedBus(bus1, busAddress)
-const fram = MB85RC.fromId(ab, id)
+const bus = /* I2CBus implementation */
+const ab = new I2CAddressedBus(bus, DEFAULT_MB85_ADDRESS)
+const fram = MB85RC.fromId(ab, DENSITY_)
 
 console.log('Fujitsu FRAM MB85', fram.busHuman, fram.densityHuman, fram.featuresHuman)
 // Fujitsu FRAM MB85 RC 256K V
--- src/mb85.js
+++ src/mb85.js
@@ -1,111 +1,138 @@
+/**
+ * @import { I2CAddressedBus, I2CBufferSource } from '@johntalton/and-other-delights'
+ */
+
 // default i2c address
-const DEFAULT_MB85_ADDRESS = 0x50;
+export const DEFAULT_MB85_ADDRESS = 0x50
 
 // datasheet max w/r times/byte
-const endurance = Math.pow(10, 12);
+export const ENDURANCE = Math.pow(10, 12)
 
-// spec defined manufactur id
-const FUJITSU_I2C_MANUFACTURER_ID = 0x0A;
+// spec defined manufacture id
+export const FUJITSU_I2C_MANUFACTURER_ID = 0x0A
 
 // density
-const D_4K  =  0x0;
+export const DENSITY_4K = 0x0
 // D_8K
 // D_16K
-const D_64K =  0x3;
+export const DENSITY_64K = 0x3
 // D_128
-const D_256K = 0x5;
-const D_512K = 0x6;
-const D_1M =   0x7;
+export const DENSITY_256K = 0x5
+export const DENSITY_512K = 0x6
+export const DENSITY_1M = 0x7
 // D_4M
 
 /**
- * Fujitsu MB85RC (i2c) managment layer
+ * @typedef {Object} MB85RCOptions
+ * @property {number} manufacturer
+ * @property {number} product
  */
-class MB85RC {
-  static from(bus, density) {
-    return new Promise((resolve, reject) => {
-      resolve(new MB85RC(bus, density, 0));
-    });
-  }
 
-  static fromId(bus, id) {
-    if(id.manufacturer !== FUJITSU_I2C_MANUFACTURER_ID) { throw new Error('Manufacturer missmatch'); }
-    // prod is split into 4bit Density and Proprietary
-    const density = id.product >> 8 & 0x0F;
-    const proprietary = id.product & 0xFF;
+/**
+ * Fujitsu MB85RC (i2c) management layer
+ */
+export class MB85RC {
+	/** @type {I2CAddressedBus} */
+	#bus
+	#density
+	#features
 
-    return Promise.resolve(new MB85RC(bus, density, proprietary));
-  }
+	/**
+	 * @param {I2CAddressedBus} bus
+	 */
+	static from(bus, density) { return new MB85RC(bus, density, 0) }
 
-  constructor(bus, density, features) {
-    this._bus = bus;
-    this._density = density;
-    this._features = features;
-  }
+	/**
+	 * @param {I2CAddressedBus} bus
+	 * @param {MB85RCOptions} id
+	 */
+	static fromId(bus, id) {
+		if (id.manufacturer !== FUJITSU_I2C_MANUFACTURER_ID) { throw new Error('Manufacturer mismatch') }
+		const density = (id.product >> 8) & 0x0F
+		const proprietary = id.product & 0xFF
 
-  get busHuman() { return Util.stringForBus('i2c'); }
-  get size() { return Util.sizeForDensity(this._density); }
-  get densityHuman() { return Util.stringForDensity(this._density); }
-  get featuresHuman() { return Util.stringForFeatures(this._features); }
+		return new MB85RC(bus, density, proprietary)
+	}
+
+	/**
+	 * @param {I2CAddressedBus} bus
+	 */
+	constructor(bus, density, features) {
+		this.#bus = bus
+		this.#density = density
+		this.#features = features
+	}
+
+	get busHuman() { return Util.stringForBus('i2c') }
+	get size() { return Util.sizeForDensity(this.#density) }
+	get densityHuman() { return Util.stringForDensity(this.#density) }
+	get featuresHuman() { return Util.stringForFeatures(this.#features) }
 
 
-  read(offset, length) {
-    return this._bus.writeBuffer(Buffer.from(Util.splitAddress(offset)))
-      .then(() => this._bus.readBuffer(length));
-  }
+	/**
+	 * @param {number} offset
+	 * @param {number} length
+	 * @param {I2CBufferSource} [target=undefined]
+	 * @returns {Promise<I2CBufferSource>}
+	 */
+	async read(offset, length, target = undefined) {
+		return this.#bus.readI2cBlock(Util.splitAddress(offset), length, target)
+	}
 
-  write(offset, buffer) {
-    return this._bus.writeBuffer(Buffer.concat([Buffer.from(Util.splitAddress(offset)), buffer]));
-  }
+	/**
+	 * @param {number} offset
+	 * @param {I2CBufferSource} buffer
+	 */
+	async write(offset, buffer) {
+		return this.#bus.writeI2cBlock(Util.splitAddress(offset), buffer)
+	}
 }
 
 /**
  * Util class
  */
-class Util {
-  static splitAddress(address) { return [(address >> 8) & 0x7F, address & 0xFF]; }
+export class Util {
+	/**
+	 * @returns {[ number, number ]}
+	 */
+	static splitAddress(address) { return [(address >> 8) & 0x7F, address & 0xFF] }
 
-  static sizeForDensity(density) {
-    switch(density) {
-      case D_4K:   return 4 * 1024 / 8; break;
-      case D_64K:  return 64 * 1024 / 8; break;
-      case D_256K: return 256 * 1024 / 8; break;
-      case D_512K: return 512 * 1024 / 8; break;
-      case D_1M:   return 1 * 1024 * 1024 / 8; break;
-      default: throw Error('i am your density: ' + density); break;
-    }
-  }
+	static sizeForDensity(density) {
+		switch (density) {
+			case DENSITY_4K: return 4 * 1024 / 8; break
+			case DENSITY_64K: return 64 * 1024 / 8; break
+			case DENSITY_256K: return 256 * 1024 / 8; break
+			case DENSITY_512K: return 512 * 1024 / 8; break
+			case DENSITY_1M: return 1 * 1024 * 1024 / 8; break
+			default: throw Error('i am your density: ' + density); break
+		}
+	}
 
-  static stringForDensity(density) {
-    switch(density) {
-      case D_4K:   return '4K'; break;
-      case D_64K:  return '64K'; break;
-      case D_256K: return '256K'; break;
-      case D_512K: return '512K'; break;
-      case D_1M:   return '1M'; break;
-      default: throw Error('i am your density: ' + density); break;
-    }
-  }
+	static stringForDensity(density) {
+		switch (density) {
+			case DENSITY_4K: return '4K'; break
+			case DENSITY_64K: return '64K'; break
+			case DENSITY_256K: return '256K'; break
+			case DENSITY_512K: return '512K'; break
+			case DENSITY_1M: return '1M'; break
+			default: throw Error('i am your density: ' + density); break
+		}
+	}
 
-  static stringForFeatures(features) {
-    switch(features) {
-      case 16: return 'V'; break;
-      case 88: return 'T'; break; // or TA, who knows
-      default: throw Error('unknown feature set ' + features); break;
-    }
-  }
+	static stringForFeatures(features) {
+		switch (features) {
+			case 16: return 'V'; break
+			case 88: return 'T'; break // or TA, who knows
+			default: throw Error('unknown feature set ' + features); break
+		}
+	}
 
-  static stringForBus(bus) {
-    switch(bus.toLowerCase()) {
-      case 'i2c': return 'RC'; break;
-      case 'spi': return 'RS'; break; // or RQ
-      case 'parallel': return 'R'; break;
-      default: throw Error('unknown bus type'); break;
-    }
-  }
+	static stringForBus(bus) {
+		switch (bus.toLowerCase()) {
+			case 'i2c': return 'RC'; break
+			case 'spi': return 'RS'; break // or RQ
+			case 'parallel': return 'R'; break
+			default: throw Error('unknown bus type'); break
+		}
+	}
 }
-
-module.exports = {
-  MB85RC,
-  DEFAULT_MB85_ADDRESS
-};
